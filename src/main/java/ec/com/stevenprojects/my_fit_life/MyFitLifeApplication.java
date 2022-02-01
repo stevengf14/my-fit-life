8cceae5069365bb141e249e343d651132feb20a3
Steven <stevengf14@gmail.com>
2022-01-31 20:37:29 -0500

    First commit - Creaging project and configuring pom

---
+++ src/main/java/ec/com/stevenprojects/my_fit_life/MyFitLifeApplication.java
@@ -0,0 +1,13 @@
+package ec.com.stevenprojects.my_fit_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/stevenprojects/my_fit_life/MyFitLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.stevenprojects.my_fit_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
